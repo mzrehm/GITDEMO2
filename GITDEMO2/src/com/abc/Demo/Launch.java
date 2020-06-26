7b5ba5861a0f69cf20cb92e22d693eac3e124ce7
mohammed <mohammed@DESKTOP-OIPI189>
2020-06-26 15:24:18 +0530

    first commit

---
+++ GITDEMO2/src/com/abc/Demo/Launch.java
@@ -0,0 +1,36 @@
+package com.abc.Demo;
+
+import java.util.Scanner;
+
+public class Launch
+{
+	public static void main(String[] args)
+	{
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Basic calculator");
+		System.out.println("select the choice to perform the action");
+		System.out.println("1. addition 2.subtraction");
+		int choice = scan.nextInt();
+		switch(choice)
+		{
+		case 1:
+			addition(2,3);
+		break;
+		case 2:
+		  subtraction(2,3);
+		break;
+		default:
+			System.out.println("Invalid choice");
+		break;
+		}
+	}
+	public static void addition(int a , int b)
+	{
+		System.out.println(a+b);
+	}
+	public static void subtraction(int a,int b)
+	{
+		System.out.println(a-b);
+	}
+
+}
